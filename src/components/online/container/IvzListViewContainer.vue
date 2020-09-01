2d222c0397efb649a8b3baf5203d2d7a4e1573fa
iteaj <iteaj@outlook.com>
2020-09-01 22:30:40 +0800

    发布1.2.2版本

---
+++ src/components/online/container/IvzListViewContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="ivz-oc-list">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "IvzListViewContainer"
+    }
+</script>
+
+<style scoped>
+
+</style>
